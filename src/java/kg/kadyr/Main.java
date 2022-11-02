5f0bace5d2675dd9b0d67905bb82f3fb7250585c
JagdpantherK <jagpanthertitanium@gmail.com>
2022-11-02 10:27:33 +0600

    Update Main.java

--- src/java/kg/kadyr/Main.java
+++ src/java/kg/kadyr/Main.java
@@ -1,4 +1,7 @@
 package kg.kadyr;
+import kg.kadyr.classes.Session;
+import kg.kadyr.menu.UserMenu;
+import kg.kadyr.utils.Sql;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -11,4 +14,17 @@ import java.sql.SQLException;
 
 
 public class Main {
+    public static Session session;
+    public static JSONObject config;
+
+
+    public static void main(String[] args) throws SQLException, IOException, ParseException {
+        JSONParser jsonParser = new JSONParser();
+        FileReader reader = new FileReader(".\\resources\\config.json");
+        config = (JSONObject) jsonParser.parse(reader);
+
+        Connection connection = Sql.connect((String) config.get("databaseName"));
+        session = new Session(connection);
+        UserMenu.main();
+    }
 }
