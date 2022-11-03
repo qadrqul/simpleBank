49366351d70cff6d97ee522759a2992bb43f2676
JagdpantherK <jagpanthertitanium@gmail.com>
2022-11-03 14:56:02 +0600

    Update UserMenu.java

--- src/java/kg/kadyr/menu/UserMenu.java
+++ src/java/kg/kadyr/menu/UserMenu.java
@@ -12,10 +12,35 @@ import java.util.Scanner;
 
 
 public class UserMenu {
-    final static Scanner scanner = new Scanner(System.in, "Cp866")
+    final static Scanner scanner = new Scanner(System.in, "Cp866");
     public static void main() throws SQLException {
     }
+    private static void loggedIn() throws SQLException {
+        System.out.println("""
+                                    
+                | Выберите желаемое действие:
+                |  1. Посмотреть баланс
+                |  2. Добавить деньги на баланс
+                |  3. Перевести деньги другому пользователю
+                |  0. Выйти из аккаунта""");
 
+        System.out.print("> ");
+        int userOperation = scanner.nextInt();
+        switch (userOperation) {
+            case 1 -> balance();
+            case 2 -> addBalance();
+            case 3 -> transfer();
+            case 0 -> {
+                System.out.println("\n| Вы успешно вышли из аккаунт!\n");
+                Main.session.unLogin();
+                main();
+            }
+            default -> {
+                System.out.println("\n| Неизвестная операция");
+                loggedIn();
+            }
+        }
+    }
     private static void balance() throws SQLException {
         Statement stmt = Main.session.getConnection().createStatement();
         Account account = Main.session.getAccount();
@@ -28,5 +53,85 @@ public class UserMenu {
 
         stmt.close();
         loggedIn();
+    }
+    private static void addBalance() throws SQLException {
+        Statement stmt = Main.session.getConnection().createStatement();
+        Account account = Main.session.getAccount();
+
+        System.out.println("\n| Введите желаемое количество");
+        System.out.print("> ");
+        long count = scanner.nextLong();
+
+        String sql = "SELECT balance FROM card WHERE number = " + account.getCard().getNumber();
+        ResultSet rs = stmt.executeQuery(sql);
+        long balance = rs.getLong("balance");
+        balance += count;
+
+        sql = "UPDATE card SET balance = " + balance + " WHERE number = " + account.getCard().getNumber();
+        stmt.execute(sql);
+        account.getCard().addBalance(count);
+
+        System.out.printf("""
+                                            
+                | На ваше баланс успешно добавлено %s денег
+                | Текущий баланс: %s%n""", count, balance);
+
+        stmt.close();
+        loggedIn();
+    }
+    private static void transfer() throws SQLException {
+        Statement stmt = Main.session.getConnection().createStatement();
+        Account account = Main.session.getAccount();
+
+        System.out.println("\n| Введите номер карты пользователя, которому хотите перевести деньги");
+        System.out.print("> ");
+        long cardNumber = scanner.nextLong();
+
+        if (!LuhnAlgorithm.numberLuhnAlgorithmCheck(cardNumber)) {
+            System.out.println("\n| Номер карты пользователя не соответствует алгоритму Луна!\n");
+            loggedIn();
+            return;
+
+        } else if (!Card.isExists(cardNumber)) {
+            System.out.println("\n| Пользователь с таким номером карты не найден!");
+            loggedIn();
+            return;
+        }
+
+        System.out.println("\n| Введите желаемое количество");
+        System.out.print("> ");
+        long count = scanner.nextLong();
+
+        String sql = "SELECT balance FROM card WHERE number = " + account.getCard().getNumber();
+        long balanceClient = stmt.executeQuery(sql).getLong("balance");
+
+        if (balanceClient < count) {
+            System.out.println("\n| Недостаточно денег на балансе!");
+            loggedIn();
+            return;
+        }
+
+        sql = "SELECT balance FROM card WHERE number = " + cardNumber;
+        long balanceRecipient = stmt.executeQuery(sql).getLong("balance");
+
+        sql = "SELECT name FROM card WHERE number = " + cardNumber;
+        String nameRecipient = stmt.executeQuery(sql).getString("name");
+
+        sql = "UPDATE card SET balance = " + (balanceClient - count) + " WHERE number = " + account.getCard().getNumber();
+        stmt.execute(sql);
+
+        sql = "UPDATE card SET balance = " + (balanceRecipient + count) + " WHERE number = " + cardNumber;
+        stmt.execute(sql);
+        account.getCard().addBalance(-count);
+
+        System.out.printf("""
+                                            
+                | Пользователю %s успешно переведено %s денег
+                | Текущий баланс: %s%n""", nameRecipient, count, balanceClient - count);
+
+        stmt.close();
+        loggedIn();
+
+        // ура, я всё сделал
     }
 }
